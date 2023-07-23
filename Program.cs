175c9cc6425de70f0daac73ad986ab98f4c70183
Nikiths1 <nikith.s1@mphasis.com>
2023-07-23 14:56:12 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BasicCSharpAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int sum = num1 + num2;
+            int difference = num1 - num2;
+            int product = num1 * num2;
+
+            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine($"Difference: {difference}");
+            Console.WriteLine($"Product: {product}");
+
+            if (num2 != 0)
+            {
+                double division = (double)num1 / num2;
+                double modulus = num1 % num2;
+
+                Console.WriteLine($"Division: {division}");
+                Console.WriteLine($"Modulus: {modulus}");
+            }
+            else
+            {
+                Console.WriteLine("Cannot divide by zero.");
+            }
+        }
+    }
+}
